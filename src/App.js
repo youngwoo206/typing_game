0de86bb21633e3edcebc3484051210378856256f
Youngwoo <youngwoo206@gmail.com>
2022-11-02 12:21:23 -0400

    framework

--- src/App.js
+++ src/App.js
@@ -1,23 +1,59 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import React, { useState, useEffect } from "react";
 
 function App() {
+  const STARTING_TIME = 5;
+
+  const [text, setText] = useState("");
+  const [timeRemaining, setTimeRemaining] = useState(STARTING_TIME);
+  const [isTimeRunning, setIsTimeRunning] = useState(false);
+  const [wordCount, setWordCount] = useState(0);
+
+  function handleChange(e) {
+    const { value } = e.target;
+    setText(value);
+  }
+
+  function calculateWordCount(text) {
+    const wordsArr = text.trim().split(" ");
+    return wordsArr.filter((word) => word !== "").length;
+  }
+
+  function startGame() {
+    setIsTimeRunning(true);
+    setTimeRemaining(STARTING_TIME);
+    setText("");
+  }
+
+  function endGame() {
+    setIsTimeRunning(false);
+    setWordCount(calculateWordCount(text));
+  }
+
+  useEffect(() => {
+    if (isTimeRunning && timeRemaining > 0) {
+      setTimeout(() => {
+        setTimeRemaining((time) => time - 1);
+      }, 1000);
+    } else if (timeRemaining === 0) {
+      endGame();
+    }
+  }, [timeRemaining, isTimeRunning]);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Typing Game</h1>
+      <h4 className="timer">Time Remaining: {timeRemaining}</h4>
+      <div className="typing_text">text here</div>
+      <textarea
+        onChange={handleChange}
+        value={text}
+        disabled={!isTimeRunning}
+      />
+      <button onClick={startGame} disabled={isTimeRunning}>
+        Start
+      </button>
+      <h4>Word count: {wordCount}</h4>
     </div>
   );
 }
